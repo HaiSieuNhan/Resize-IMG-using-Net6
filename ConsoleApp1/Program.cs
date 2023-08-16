6bf74e5332f0595a301beb97f9bfb69c5bf8308b
HaiDV <haidv@sunhouse.com.vn>
2023-08-16 17:09:01 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var xxx = sum(1, 2);
+Console.WriteLine($"{xxx}");
+int sum(int a, int b)
+{
+    return a + b;
+}
---
+++ HaiDV/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SkiaSharp;
+using System.Threading.Tasks.Dataflow;
+using static SkiaSharp.SKBitmapResizeMethod;
+
+Console.WriteLine("Hello, World!");
+var byteImg = ImageToByteArray("C:\\Users\\Dao Van Hai\\Desktop\\HAIDV\\HaiDV\\HaiDV\\pexels-irina-iriser-1379640.jpg");
+var dataSave = ResizeImg(byteImg);
+SaveFile(dataSave);
+Console.WriteLine("OKE OKE");
+
+SKData ResizeImg(byte[] byteImg)
+{
+    var original = SKBitmap.Decode(byteImg).Resize(new SKImageInfo(200, 300), Lanczos3);
+
+    var image = SKImage.FromBitmap(original).Encode(SKEncodedImageFormat.Png, 90);
+    return image;
+}
+
+
+byte[] ImageToByteArray(string filePath)
+{
+    byte[] fileByteArray = File.ReadAllBytes(filePath);
+    return fileByteArray;
+}
+
+void SaveFile(SKData input)
+{
+    string pathSave = "D:\\Work\\ConsoleTeleBot" + Guid.NewGuid() + ".png";
+    File.WriteAllBytes(pathSave, input.ToArray());
+}
